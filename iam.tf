f67c2afe14c800be24e63844d726ccb725b227c0
Rogier <rogier@fortytwo.nl>
2020-12-12 22:56:43 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 module "iam_role" {
-  source = "git::https://github.com/fortytwosecurity/tf-aws-ec2.git"
+  source = "git::https://github.com/fortytwosecurity/tf-aws-iam-role-common.git"
 
   iam_role_name = "${var.tags["environment"]}-${var.tags["role"]}"
 }
